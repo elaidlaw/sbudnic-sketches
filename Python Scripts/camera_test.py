c84e8965812afe030e8f1194b0c71331404b4e67
Eliot Laidlaw <laidlaw.eliot@gmail.com>
2022-02-20 01:08:04 -0500

    board assembly updates

--- Python Scripts/camera_test.py
+++ Python Scripts/camera_test.py
@@ -6,9 +6,19 @@ import time
 
 arduino = serial.Serial(port='COM8', baudrate=9600, timeout=.1)
 
-with open('image.jpg', 'wb+') as f:
+with open('image.bin', 'wb+') as f:
     start = time.time()
-    while time.time() - start < 120:
+    while time.time() - start < 60:
         new = arduino.readall()
         print(len(new))
         f.write(new)
+        # if len(new) > 0:
+        #     break
+
+# .045 transmit
+# .150 camera
+# 28FBB60D000000DA
+# 1k (brown black red) for light sensors
+# 4.7k (yellow purple red) for temp
+# 10k for LLC
+# 100k for dep switches
