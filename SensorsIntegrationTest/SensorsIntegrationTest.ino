c0dd677937a7193bf7cf1f799a682ff02498936e
Filip Kierzenka <filip@filips-mbp-2.devices.brown.edu>
2022-03-01 20:32:42 -0500

    need to pull

--- SensorsIntegrationTest/SensorsIntegrationTest.ino
+++ SensorsIntegrationTest/SensorsIntegrationTest.ino
@@ -28,44 +28,45 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  recobj->enable();
-  int num;
-  while((num = recobj->get_state()) == REC_WAIT){
-    Serial.println("waiting");
-    delay(500);
-  }
-//  Serial.println(num);
-  recobj->disable();
-//  Serial.println("picture done");
-//  Serial.println();
-  int d = recobj->get_output_pos();
-  int z;
-  for(z=0;z<d;z++){
-    Serial.write(data[z]);
-    delay(10);
-  }
-  delay(2000);
-
-  radioLink->enable();
-  delay(1000);
-  radioLink->disable();
-  delay(1000);
-//  Serial.println("radio done");
+//  recobj->enable();
+//  int num;
+//  while((num = recobj->get_state()) == REC_WAIT){
+//    Serial.println("waiting");
+//    delay(500);
+//  }
+////  Serial.println(num);
+//  recobj->disable();
+////  Serial.println("picture done");
+////  Serial.println();
+//  int d = recobj->get_output_pos();
+//  int z;
+//  for(z=0;z<d;z++){
+//    Serial.write(data[z]);
+//    delay(10);
+//  }
+//  delay(2000);
+//
+//  radioLink->enable();
+//  delay(1000);
+//  radioLink->disable();
+//  delay(1000);
+////  Serial.println("radio done");
 
   sensors->enable();
   char sensorData[DATA_LENGTH] = {0};
   sensors->readAllSensors(sensorData);
   delay(1000);
-  sensorData[DATA_LENGTH] = {0};
-  sensors->readAllSensors(sensorData);
-  sensors->disable();
+//  sensorData[DATA_LENGTH] = {0};
+//  sensors->readAllSensors(sensorData);
+//  sensors->disable();
 //  Serial.println("sensors done");
+  int z;
   for(z=0; z<DATA_LENGTH;z++){
-//    Serial.print(sensorData[z]);
+    Serial.print(sensorData[z]);
   }
-//  Serial.println();
+  Serial.println();
 
-  delay(15000);
+//  delay(500);
   
 }
 
